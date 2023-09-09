fa38a4aa5028ba075cc0cfe87aa4ff015d700b6b
christopher-distasio <stahzz1970@gmail.com>
2023-09-08 22:23:36 -0500

    Completes M3 HW sign-up-and-log-in

--- backend/migrations/20230906205526-add-user-password.js
+++ backend/migrations/20230906205526-add-user-password.js
@@ -1,26 +1,13 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    return queryInterface.addColumn('users', 'password_digest', 
-    {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'password_digest', {
       type: Sequelize.DataTypes.STRING
     })
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('users', 'password_digest')
   }
 };
