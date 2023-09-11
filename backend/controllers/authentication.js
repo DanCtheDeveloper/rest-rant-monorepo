57771a3336983d0b9d97d8afeced1a02ac52dca6
christopher-distasio <stahzz1970@gmail.com>
2023-09-11 13:27:53 -0500

    Prepares backend for sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -13,8 +13,26 @@ router.post('/', async (req, res) => {
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
         res.status(404).json({ message: `Could not find a user with the provided username and password` })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        // let user = await User.findOne({
+        //     where: {
+        //         userId: ___
+        //     }
+
+        // })
+
+        // res.json(user)
+    } catch (error) {
+        res.json(null)
+    }
+})
+
 
 module.exports = router
